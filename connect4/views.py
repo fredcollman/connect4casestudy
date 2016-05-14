396248d44109b8e5c062d1ea8c452c72fc5f0ca0
Fred Collman <fredcollman@gmail.com>
2016-05-14 19:54:38 +0100

    Set up standard django.contrib.auth

--- connect4/views.py
+++ connect4/views.py
@@ -1,5 +1,5 @@
-
-
+from django.contrib.auth.decorators import login_required
+import django.contrib.auth.views as auth_views
 from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404, get_list_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect, JsonResponse
@@ -7,21 +7,9 @@ from connect4 import models
 
 
 # Create your views here.
-def login(request):
-    """
-    Write your login view here
-    :param request:
-    :return:
-    """
-    pass
+login = auth_views.login
+logout = auth_views.logout
 
-def logout(request):
-    """
-    write your logout view here
-    :param request:
-    :return:
-    """
-    pass
 
 def signup(request):
     """
@@ -31,6 +19,8 @@ def signup(request):
     """
     pass
 
+
+@login_required
 def games(request):
     """
     Write your view which controls the game set up and selection screen here
@@ -40,6 +30,7 @@ def games(request):
     pass
 
 
+@login_required
 def play(request):
     """
     write your view which controls the gameplay interaction w the web layer here
